7128d0dc035e44167bd90cbdd6740b4ead3a9651
hu <312977493@qq.com>
2024-07-31 15:52:56 +0800

    配置 ElementPlus 插件与自动导入插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            imports: [
+                'vue',
+                'vue-router',
+                // 你可以在这里添加更多需要自动引入的库
+            ],
+            resolvers: [ElementPlusResolver()],
+            dts: 'src/auto-imports.d.ts', // 自动生成的类型声明文件路径
+        }),
+        Components({
+            resolvers: [
+                ElementPlusResolver()
+            ],
+            dts: 'src/components.d.ts', // 自动生成的类型声明文件路径
+        }),
+    ],
 })
