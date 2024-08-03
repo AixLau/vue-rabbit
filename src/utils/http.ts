99893e65686253c2721f421c65b35edca108eeeb
aix <aixlau@outlook.com>
2024-08-03 23:10:10 +0800

    token 失效拦截

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,7 @@
 // axios 基本封装
 
 import {ElMessage} from "element-plus";
+import router from "@/router";
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -8,9 +9,9 @@ const httpInstance = axios.create({
 })
 
 // 请求拦截器
+const userStore = useUserStore()
 httpInstance.interceptors.request.use(
     config => {
-        const userStore = useUserStore()
         const token = userStore.userInfo.token
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
@@ -23,6 +24,10 @@ httpInstance.interceptors.request.use(
 httpInstance.interceptors.response.use(
     response => response.data,
     error => {
+        if (error.response.status === 401) {
+            userStore.clearUserInfo()
+            router.push('/')
+        }
         ElMessage({type: 'warning', message: error.response?.data?.message || '请求失败'});
         return Promise.reject(error);  // 确保在发生错误时抛出错误
     }
