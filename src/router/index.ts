0486eeeedad933a2a647bf45bbb7247b0bc30dbe
aix <aixlau@outlook.com>
2024-08-04 15:02:59 +0800

    本地购物车-列表购物车渲染商品

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,24 +5,18 @@ const routes = [
         path: '/',
         component: () => import('@/views/layout/index.vue'),
         children: [
-            {
-                path: '',
-                component: () => import('@/views/home/index.vue'),
-            },
-            {
-                path: 'category/:id',
-                component: () => import('@/views/category/index.vue')
-            },
-            {
-                path: 'category/sub/:id',
-                component: () => import('@/views/sub-category/index.vue')
-            }, {path: 'detail/:id', component: () => import('@/views/detail/index.vue')}
+            {path: '', component: () => import('@/views/home/index.vue'),},
+            {path: 'category/:id', component: () => import('@/views/category/index.vue')},
+            {path: 'category/sub/:id', component: () => import('@/views/sub-category/index.vue')},
+            {path: 'detail/:id', component: () => import('@/views/detail/index.vue')},
+            {path: '/cartlist', component: () => import('@/views/cart/index.vue'),},
         ]
     },
     {
         path: '/login',
         component: () => import('@/views/login/index.vue'),
     },
+
 ];
 
 const router = createRouter({
